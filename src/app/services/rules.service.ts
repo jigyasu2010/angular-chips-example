94861bc970d1b288dddd917eaffece8bf8603ec7
jigyasu2010 <jigyasu2010@hotmail.com>
2019-01-09 19:55:08 +0530

    Add files via upload

--- src/app/services/rules.service.ts
+++ src/app/services/rules.service.ts
@@ -1,1 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class RulesService {
+
+  private url = "https://api.myjson.com/bins/c6298";
+  private saveUrl = "http://dasdasd";
+
+  constructor(private http: HttpClient) { }
+
+  getRules(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  saveRules(payload): Observable<any> {
+    return this.http.post(this.saveUrl, payload)
+  }
+
+}
